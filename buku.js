eb2129c0508bd0f3a5fe39bb76800202d618088d
nailafauzia <nailafauzia56@gmail.com>
2026-07-20 09:37:02 +0700

    feat: menyelesaikan task 8 buku berdasarkan negara

--- buku.js
+++ buku.js
@@ -16,3 +16,28 @@ authors.forEach(author => {
   console.log(`Jumlah Buku: ${bukuPenulis.length}\n`);
   
 });
+
+// --- Task 8: Buku Berdasarkan Negara ---
+
+// Kita membuat function agar nama negara bisa diubah-ubah dengan mudah (dinamis)
+function tampilkanBukuBerdasarkanNegara(negara) {
+  books.forEach(book => {
+    // Cari penulis dari buku ini
+    const author = authors.find(a => a.id === book.authorId);
+    
+    // Jika penulis ditemukan DAN negara penulis sama dengan parameter 'negara'
+    if (author && author.country === negara) {
+      console.log(book.title);
+      console.log(author.name);
+      console.log(`${author.country}\n`);
+    }
+  });
+}
+
+// Memanggil fungsi untuk menampilkan buku dari Indonesia sesuai output yang diharapkan
+console.log("=== Buku dari Indonesia ===");
+tampilkanBukuBerdasarkanNegara("Indonesia");
+
+// Menguji fungsi untuk negara USA (seperti yang diminta instruksi)
+console.log("=== Buku dari USA ===");
+tampilkanBukuBerdasarkanNegara("USA");
